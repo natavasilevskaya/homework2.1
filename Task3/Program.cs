e9193f15ddf8b0ee1439cb91458c6d910fa3f729
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-07-16 00:46:04 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Написать программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void FindDayOfWeek (int n) // метод по введеному числу выводит день недели
+{
+if (n <= 5)
+{
+    Console.WriteLine("It is not a day off!(");
+}
+if (n == 6 || n == 7)
+{
+    Console.WriteLine("It is a day off!");
+}
+else
+{
+    if (n>7)
+    {
+        Console.WriteLine("It is not correct number!");
+    }
+}
+}
+
+Console.WriteLine("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+FindDayOfWeek(num);
+
