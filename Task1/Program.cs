21da40ad0a7cad57046075c05fd0f133a301f070
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-07-15 23:39:15 +0300

    Task1 file added

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void SecondDigit (int n)
+{
+    int twodig = n/10;
+    int result = twodig % 10;
+    Console.WriteLine("Second digit is" + result);
+}
+
+Console.WriteLine("Input three digit number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+SecondDigit(n1);
